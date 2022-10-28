155f0f5c677de2e0718577e3c8adb4e711bb6bf3
rdh tiwari <tiwari.rdhtiwari@gmail.com>
2022-10-28 22:26:49 +0530

    28oct

---
+++ Class/Linked_list/Class_19.py
@@ -0,0 +1,7 @@
+# bit manipulation
+# left shift vs right shift
+# Reversal of linked list
+# cycle detection in linked list: Floyed Cycle detection or tortoise hare Algorithms or Slow fast Algorithms
+
+
+
